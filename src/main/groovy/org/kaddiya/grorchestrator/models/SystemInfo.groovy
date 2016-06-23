5db5fa5cdb6a14bc5e7fe8ddb2697438b264c097
kaddiya <ameya.kulkarni@weboniselab.com>
2016-06-23 19:25:11 +0530

    simplified the SystemInfo class

--- src/main/groovy/org/kaddiya/grorchestrator/models/SystemInfo.groovy
+++ src/main/groovy/org/kaddiya/grorchestrator/models/SystemInfo.groovy
@@ -1,13 +1,13 @@
 package org.kaddiya.grorchestrator.models
 
-import groovy.transform.Canonical
+import groovy.transform.Immutable
 
 
 /**
  * Created by Webonise on 19/03/16.
  */
-@Canonical
+@Immutable
 class SystemInfo {
     String name
-    String grorFile
+
 }

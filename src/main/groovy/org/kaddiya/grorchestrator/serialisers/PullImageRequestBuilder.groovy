124c6b81ba393ac8ce8c8ed9e773e9a2e3351c7c
kaddiya <kaddiya@gmail.com>
2016-06-24 18:40:26 +0530

    Changes for pull images request builder

---
+++ src/main/groovy/org/kaddiya/grorchestrator/serialisers/PullImageRequestBuilder.groovy
@@ -0,0 +1,8 @@
+package org.kaddiya.grorchestrator.serialisers
+
+/**
+ * Created by Webonise on 24/06/16.
+ */
+interface PullImageRequestBuilder {
+
+}
---
+++ src/main/groovy/org/kaddiya/grorchestrator/serialisers/impl/PullImageRequestBuilderImpl.groovy
@@ -0,0 +1,7 @@
+package org.kaddiya.grorchestrator.serialisers.impl
+
+/**
+ * Created by Webonise on 24/06/16.
+ */
+class PullImageRequestBuilderImpl {
+}

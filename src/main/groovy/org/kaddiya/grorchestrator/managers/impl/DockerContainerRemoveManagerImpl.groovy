631c6e196bcb3df9654ab460966f2e02c82e4070
kaddiya <kaddiya@gmail.com>
2016-07-14 21:13:02 +0530

    Added the delete funcitonality

--- src/main/groovy/org/kaddiya/grorchestrator/managers/impl/DockerContainerRemoveManagerImpl.groovy
+++ src/main/groovy/org/kaddiya/grorchestrator/managers/impl/DockerContainerRemoveManagerImpl.groovy
@@ -1,31 +1,34 @@
 package org.kaddiya.grorchestrator.managers.impl
 
 import com.google.inject.Inject
+import com.google.inject.assistedinject.Assisted
+import groovy.transform.CompileStatic
 import groovyx.net.http.Method
+import net.sf.json.JSON
 import org.kaddiya.grorchestrator.managers.DockerContainerRemoveManager
 import org.kaddiya.grorchestrator.managers.DockerRemoteAPI
 import org.kaddiya.grorchestrator.models.core.Instance
 import org.kaddiya.grorchestrator.models.remotedocker.responses.DockerContainerCreationResponse
+import groovyx.net.http.RESTClient
 
 /**
  * Created by Webonise on 14/07/16.
  */
+@CompileStatic
 class DockerContainerRemoveManagerImpl extends DockerRemoteAPI implements DockerContainerRemoveManager {
 
     @Inject
-    DockerContainerRemoveManagerImpl(Instance instance) {
+    DockerContainerRemoveManagerImpl(@Assisted Instance instance) {
         super(instance)
     }
 
     @Override
     void removeContainer() {
+        def url = "http://" + this.instance.host.ip + ":" + this.instance.host.dockerPort
+        RESTClient new_client = new RESTClient(url)
         println("deleting the instance with $instance.name")
         this.tryCatchClosure {
-            DockerContainerCreationResponse response
-            response = this.client.request(
-                    method:Method.DELETE,{ req ->
-                    path : "/container/$instance.name"
-            })
+            new_client.delete(path: "/containers/$instance.name")
         }
     }
 }

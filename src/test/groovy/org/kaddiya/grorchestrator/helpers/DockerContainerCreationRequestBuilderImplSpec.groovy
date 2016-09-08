ee75df0c9d2c7343254dfe1f9e37814dccb2851f
kaddiya <kaddiya@gmail.com>
2016-09-08 19:52:31 +0530

    Added the tests

--- src/test/groovy/org/kaddiya/grorchestrator/helpers/DockerContainerCreationRequestBuilderImplSpec.groovy
+++ src/test/groovy/org/kaddiya/grorchestrator/helpers/DockerContainerCreationRequestBuilderImplSpec.groovy
@@ -10,15 +10,15 @@ import spock.lang.Specification
 /**
  * Created by Webonise on 13/07/16.
  */
-class DockerConatinerCreationRequestBuilderImplSpec extends Specification {
+class DockerContainerCreationRequestBuilderImplSpec extends Specification {
 
 
     @Shared
     DockerContainerCreationRequestBuilder fixture = new DockerContainerCreationRequestBuilderImpl()
 
-    def "getPortMappingsFromInstance should return a proper port mapping as expected by the Docker remote api"() {
+    def "getPortMappingsFromInstance should return a proper port mapping as expected by the Docker remote api V 1.19"() {
         given:
-        Instance instance = getDummyInstance()
+        Instance instance = getDummyInstanceForVOnePointNineteen()
         when:
         Map<String, Object> result = fixture.getPortMappingsFromInstance(instance)
         then:
@@ -26,9 +26,9 @@ class DockerConatinerCreationRequestBuilderImplSpec extends Specification {
         assert "{\"22/tcp\":{}}" == (JsonOutput.toJson(result))
     }
 
-    def "getVolumes should return a proper volume mapping as expected by the Docker remote api"() {
+    def "getVolumes should return a proper volume mapping as expected by the Docker remote api V 1.19"() {
         given:
-        Instance instance = getDummyInstance()
+        Instance instance = getDummyInstanceForVOnePointNineteen()
         when:
         Map<String, Object> result = fixture.getVolumes(instance)
         println(result.toMapString())
@@ -36,9 +36,9 @@ class DockerConatinerCreationRequestBuilderImplSpec extends Specification {
         assert "{\"/data-1\":{},\"/data-2\":{}}" == (JsonOutput.toJson(result))
     }
 
-    def "getEnvironmentMappings should return a proper list of environment variables and values as expected by the Docker remote api"() {
+    def "getEnvironmentMappings should return a proper list of environment variables and values as expected by the Docker remote api V1.19"() {
         given:
-        Instance instance = getDummyInstance()
+        Instance instance = getDummyInstanceForVOnePointNineteen()
         when:
         List<String> result = (fixture as DockerContainerCreationRequestBuilderImpl).getEnvironmentMappings(instance)
         then:
@@ -46,9 +46,18 @@ class DockerConatinerCreationRequestBuilderImplSpec extends Specification {
     }
 
 
-    Instance getDummyInstance() {
+    Instance getDummyInstanceForVOnePointNineteen() {
         return new Instance("redis.proof.com", "redis", "latest",
-                new Host("127.0.0.1", "redis-vm-1", 2376),
+                new Host("127.0.0.1", "redis-vm-1", 2376,"http","1.7.1.","1.19"),
+                Collections.unmodifiableMap(["/home/deploy/cache-data-1": "/data-1", "/home/deploy/cache-data-2": "/data-2"]), Collections.unmodifiableMap([10222: 22]) as Map<Integer, Integer>,
+                Collections.unmodifiableMap([:]),
+                Collections.unmodifiableMap(["env": "test", "number": "3"]
+                ))
+    }
+
+    Instance getDummyInstanceForVOnePointTwentyFour() {
+        return new Instance("redis.proof.com", "redis", "latest",
+                new Host("127.0.0.1", "redis-vm-1", 2376,"http","1.12.","1.24"),
                 Collections.unmodifiableMap(["/home/deploy/cache-data-1": "/data-1", "/home/deploy/cache-data-2": "/data-2"]), Collections.unmodifiableMap([10222: 22]) as Map<Integer, Integer>,
                 Collections.unmodifiableMap([:]),
                 Collections.unmodifiableMap(["env": "test", "number": "3"]

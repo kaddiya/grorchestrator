8fc1cd050f7e7b2f3205b453ede5928f1776fd8f
kaddiya <kaddiya@gmail.com>
2016-09-16 14:56:46 +0530

    Changes for the dont care condition of hostConfig

--- src/main/groovy/org/kaddiya/grorchestrator/helpers/impl/HostConfigBuilderImpl.groovy
+++ src/main/groovy/org/kaddiya/grorchestrator/helpers/impl/HostConfigBuilderImpl.groovy
@@ -12,14 +12,7 @@ import org.kaddiya.grorchestrator.models.remotedocker.requests.HostConfig
 class HostConfigBuilderImpl implements HostConfigBuilder {
 
     HostConfig constructHostConfig(Instance instance) {
-        HostConfig config
-        if(instance.host.apiVersion  == '1.19'){
-            config = new HostConfig()
-        }
-        else{
-            config = new HostConfig(getBinds(instance), getLinks(instance), getPortBindings(instance), getExtraHostsMapping(instance))
-        }
-
+        HostConfig config = new HostConfig(getBinds(instance), getLinks(instance), getPortBindings(instance), getExtraHostsMapping(instance))
         return config
     }
 

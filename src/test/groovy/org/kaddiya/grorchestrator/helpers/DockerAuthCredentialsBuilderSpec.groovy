4e56045b56d75c75a0117c16b055ba49a9b72990
kaddiya <kaddiya@gmail.com>
2017-03-01 17:06:09 +0530

    Changes for making the grorchestator project as core

--- src/test/groovy/org/kaddiya/grorchestrator/helpers/DockerAuthCredentialsBuilderSpec.groovy
+++ src/test/groovy/org/kaddiya/grorchestrator/helpers/DockerAuthCredentialsBuilderSpec.groovy
@@ -10,19 +10,5 @@ import spock.lang.Specification
  */
 class DockerAuthCredentialsBuilderSpec extends Specification {
 
-    @Shared
-    DockerhubAuthCredentialsBuilder builder = new DockerhubAuthCredentialsBuilder()
-
-
-    def "getbase64EncodedValueForCredentials should return a proper base64 encoded String"() {
-        given:
-
-        DockerHubAuth acutalAuth = new DockerHubAuth("default-key", "username", "password", "email@example.com", "")
-        when:
-        String result = builder.getbase64EncodedValueForCredentials(acutalAuth)
-        then:
-        assert result == "eyJwYXNzd29yZCI6InBhc3N3b3JkIiwia2V5IjoiZGVmYXVsdC1rZXkiLCJ1c2VybmFtZSI6InVzZXJuYW1lIiwiYXV0aCI6IiIsImVtYWlsIjoiZW1haWxAZXhhbXBsZS5jb20ifQ=="
-    }
-
 
 }

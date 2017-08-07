0134f0b0938997e4ef9741f7af1f2360b3d83344
kaddiya <kaddiya@gmail.com>
2017-08-08 00:28:18 +0530

    Changes for redeploying

--- src/main/groovy/org/kaddiya/grorchestrator/models/core/SupportedContainerActions.java
+++ src/main/groovy/org/kaddiya/grorchestrator/models/core/SupportedContainerActions.java
@@ -4,5 +4,5 @@ package org.kaddiya.grorchestrator.models.core;
  * Created by Webonise on 24/06/16.
  */
 public enum SupportedContainerActions {
-    PULL, RUN, KILL, REMOVE, STATUS
+    PULL, RUN, KILL, REMOVE, STATUS,REDEPLOY
 }
